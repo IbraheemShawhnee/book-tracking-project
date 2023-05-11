7c43c49d849755878be0329b51754ab7f6a5eb07
IbraheemShawhnee <i.shawahneh@student.aaup.edu>
2023-05-11 17:07:13 +0300

    Initialize the project with the packages required. Firebase, tailwind, Charts and react-router-dom.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@mui/material";
+function App() {
+	return (
+		<>
+			<h1 className="text-sm font-medium text-red-900">Hello world!</h1>
+			<Button variant="text" className="bg-black text-red-600">
+				Texhh
+			</Button>
+		</>
+	);
+}
+
+export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBSZukp9HUMdxJFn7ohumUGdiLrECPPjAg",
+	authDomain: "library-bf484.firebaseapp.com",
+	projectId: "library-bf484",
+	storageBucket: "library-bf484.appspot.com",
+	messagingSenderId: "756572754345",
+	appId: "1:756572754345:web:0f7646f27405f481a674b8",
+	measurementId: "G-YY0S46WXFL",
+};
+
+const app = initializeApp(firebaseConfig);
