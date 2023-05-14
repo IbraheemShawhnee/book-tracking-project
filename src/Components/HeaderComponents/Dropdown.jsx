73b42c84bfc53a4b8df1db077840bdb0f2c02c2a
IbraheemShawhnee <i.shawahneh@student.aaup.edu>
2023-05-14 13:39:17 +0300

    Added on the Profile Pic Dropdown menu

--- src/Components/HeaderComponents/Dropdown.jsx
+++ src/Components/HeaderComponents/Dropdown.jsx
@@ -5,28 +5,35 @@ import Person2Icon from "@mui/icons-material/Person2";
 import StackedLineChartIcon from "@mui/icons-material/StackedLineChart";
 import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
 import Icon from "../Icon/index";
-
+import { Box, Typography } from "@mui/material";
 const Dropdown = ({ name }) => {
 	return (
-		<motion.div>
+		<motion.div
+			className={`hidden xl:block absolute origin-top-right top-9 right-0 py-2 z-10 min-w-full rounded-md bg-white dark:bg-gray-800 shadow-lg ring-3 ring-white focus:outline-none`}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+		>
 			<Box className="py-1">
 				<Link
 					to={`/profile/${name}`}
-					className="flex items-center gap-1 w-full px-4 p-2 text-left text-sm dark:text-white hover:text-gray-500 dark:hover:text-gray-300 "
+					className="flex items-center gap-1 w-full px-4 p-2 text-left text-sm text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-300 "
 				>
-					<Icon icon={<Person2Icon />} />
+					<Icon icon={<Person2Icon color="primary" />} />
+					<Typography>Profile</Typography>
 				</Link>
 				<Link
 					to={`/profile/${name}/statistics`}
-					className="flex items-center gap-1 w-full px-4 py-2 text-left text-sm dark:text-white  hover:text-gray-500 dark:hover:text-gray-300"
+					className="flex items-center gap-1 w-full px-4 py-2 text-left text-sm text-black dark:text-white  hover:text-gray-500 dark:hover:text-gray-300"
 				>
-					<Icon icon={<StackedLineChartIcon />} />
+					<Icon icon={<StackedLineChartIcon color="primary" />} />
+					<Typography>Statistics</Typography>
 				</Link>
 				<Link
 					to={`/settings`}
-					className="flex items-center gap-1 w-full px-4 py-2 text-left text-sm dark:text-white  hover:text-gray-500 dark:hover:text-gray-300"
+					className="flex items-center gap-1 w-full px-4 py-2 text-left text-sm text-black dark:text-white  hover:text-gray-500 dark:hover:text-gray-300"
 				>
-					<Icon icon={<SettingsApplicationsIcon />} />
+					<Icon icon={<SettingsApplicationsIcon color="primary" />} />
+					<Typography>Settings</Typography>
 				</Link>
 			</Box>
 		</motion.div>
