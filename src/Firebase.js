8b1dd3a981be15e5e422369f1a3c482ffa9974c9
IbraheemShawhnee <i.shawahneh@student.aaup.edu>
2023-05-14 19:51:19 +0300

    Update Database

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,13 +8,15 @@ import {
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDA-tFchH_OJ8XgmpRERtbDKKlJI5jamSA",
-	authDomain: "book-library-d3d78.firebaseapp.com",
-	projectId: "book-library-d3d78",
-	storageBucket: "book-library-d3d78.appspot.com",
-	messagingSenderId: "988692225066",
-	appId: "1:988692225066:web:db9cef15b26897641fb753",
+	apiKey: "AIzaSyBSZukp9HUMdxJFn7ohumUGdiLrECPPjAg",
+	authDomain: "library-bf484.firebaseapp.com",
+	projectId: "library-bf484",
+	storageBucket: "library-bf484.appspot.com",
+	messagingSenderId: "756572754345",
+	appId: "1:756572754345:web:0f7646f27405f481a674b8",
+	measurementId: "G-YY0S46WXFL",
 };
+
 const provider = new GoogleAuthProvider();
 
 const app = initializeApp(firebaseConfig);
