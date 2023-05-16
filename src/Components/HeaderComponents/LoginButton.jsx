48775b1c744d2ac606106b7bc375dafe2d175640
Ibraheem Shawhnee <116644664+IbraheemShawhnee@users.noreply.github.com>
2023-05-16 18:52:08 +0300

    Updates

--- src/Components/HeaderComponents/LoginButton.jsx
+++ src/Components/HeaderComponents/LoginButton.jsx
@@ -1,43 +1,35 @@
-import Dropdown from "./Dropdown";
-import React, { useState } from "react";
-import { signIn } from "../../Firebase";
+import React from "react";
+import { Box, Button } from "@mui/material";
 import useUserData from "../../Hooks/useUserData";
+import { signIn } from "../../Firebase";
+import Dropdown from "./Dropdown";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Icon from "../Icon";
 const LoginButton = () => {
 	const [name, profilePic] = useUserData();
-	const [isOpen, setIsOpen] = useState(false);
-
 	return (
 		<>
 			{name ? (
-				<div
-					className="relative group"
-					onMouseEnter={() => setIsOpen(true)}
-					onMouseLeave={() => setIsOpen(false)}
-				>
-					<div className="flex items-center gap-1 hover:cursor-pointer">
+				<Box className="relative group">
+					<Box className="flex items-center gap-1 hover:cursor-pointer">
 						<img
 							className="w-9 h-9 rounded-sm"
 							src={profilePic}
 							alt="Profile Picture"
-							referrerPolicy="no-referrer"
 						/>
-						<svg
-							className="hidden xl:block w-5 h-5 fill-gray-400 group-hover:fill-gray-600
-              dark:fill-gray-600 dark:group-hover:fill-gray-400 mt-1"
-							viewBox="0 0 24 24"
-						>
-							<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-						</svg>
-					</div>
-					{isOpen && <Dropdown name={name} />}
-				</div>
+						<Icon icon={<KeyboardArrowDownIcon />} />
+					</Box>
+					<Box className="">
+						<Dropdown name={name} />
+					</Box>
+				</Box>
 			) : (
-				<button
+				<Button
 					className="px-6 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
 					onClick={signIn}
 				>
 					Login/Register
-				</button>
+				</Button>
 			)}
 		</>
 	);
