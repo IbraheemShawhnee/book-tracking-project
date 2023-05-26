c455722f1090caa834b8e12c8cfc0cdb7884759b
Ibraheem Shawhnee <116644664+IbraheemShawhnee@users.noreply.github.com>
2023-05-26 15:53:01 +0300

    Some improvment in the styling

--- src/Components/ProfileComponents/BookList.jsx
+++ src/Components/ProfileComponents/BookList.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Typography, Button, Box } from "@mui/material";
-import { auth, isUserSignedIn } from "../../Firebase";
+import { Typography, Box } from "@mui/material";
 import MessageIcon from "@mui/icons-material/Message";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
-const BookList = ({ books, title, sort, openModal, username }) => {
+const BookList = ({ books, title, sort, openModal }) => {
 	const filtered = books
 		.filter((book) => book.status === title)
 		.sort((prevBook, book) => {
-			// if property is a number uses 1st method, else uses 2nd
 			return (
 				book[sort.toLowerCase()] - prevBook[sort.toLowerCase()] ||
 				(book[sort.toLowerCase()] < prevBook[sort.toLowerCase()] ? 1 : -1)
 			);
 		});
-	const authorized =
-		isUserSignedIn() && username === auth.currentUser.displayName;
 
 	if (filtered.length === 0) return <></>;
 
@@ -28,39 +23,25 @@ const BookList = ({ books, title, sort, openModal, username }) => {
 			<Box>
 				{filtered.map((book) => {
 					return (
-						<div
+						<Box
 							key={book.id}
-							className="relative group-one flex items-center p-3 pr-6 gap-3 hover:bg-gray-800 hover:text-white text-base sm:text-lg"
+							className="relative flex items-center p-3 pr-6 gap-3 hover:bg-gray-800 hover:text-white text-base sm:text-lg"
 						>
-							{/* full image on hover */}
-							<img
-								src={book.imageUrl}
-								className="hidden lg:group-one-hover:block absolute -left-36 rounded"
-								alt="Book Cover"
-							/>
 							<Box className="w-11 h-11 sm:w-12 sm:h-12 shrink-0">
-								{/* small image for list */}
 								<img
-									className={`rounded object-cover object-center w-full max-h-full group-one-hover:hidden`}
+									className={`rounded object-cover object-center w-full max-h-full group-hover:bg-gray-700`}
 									src={book.imageUrl}
 									alt="Book Cover"
+									onClick={() => openModal(book)}
 								/>
-								{/* Edit button on hover */}
-								{authorized && (
-									<Button
-										className="rounded w-full h-full hidden bg-gray-700 group-one-hover:flex items-center justify-center"
-									>
-										<MoreHorizIcon />
-									</Button>
-								)}
 							</Box>
 							<Typography className="mr-auto line-clamp-2">
 								{book.title}
 							</Typography>
 							{book.notes ? (
-								<Box className="group-two relative">
+								<Box className="relative group">
 									<MessageIcon />
-									<Typography className="z-10 hidden md:group-two-hover:block absolute rounded text-base p-4 -right-1/2 w-max max-w-xs bg-gray-800">
+									<Typography className="z-10 hidden md:hidden group-hover:block absolute rounded text-base p-4 -right-1/2 w-max max-w-xs bg-gray-800">
 										{book.notes}
 									</Typography>
 								</Box>
@@ -71,7 +52,7 @@ const BookList = ({ books, title, sort, openModal, username }) => {
 									<span>{book.rating}/10</span>
 								</Typography>
 							) : null}
-						</div>
+						</Box>
 					);
 				})}
 			</Box>
