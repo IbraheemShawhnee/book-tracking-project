d84b0676fa36ca754501394a6eadf32dbf55ad07
Ibraheem Shawhnee <116644664+IbraheemShawhnee@users.noreply.github.com>
2023-06-08 17:05:02 +0300

    For build purpose

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,30 @@
-// vite.config.ts
+// // vite.config.ts
+// import { defineConfig } from "vite";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(() => ({
+// 	esbuild: {
+// 		loader: "tsx", // OR "jsx"
+// 		include: [
+// 			// Add this for business-as-usual behaviour for .jsx and .tsx files
+// 			"src/**/*.jsx",
+// 			"src/**/*.tsx",
+// 			"node_modules/**/*.jsx",
+// 			"node_modules/**/*.tsx",
+
+// 			// Add the specific files you want to allow JSX syntax in
+// 			"src/LocalJsxInJsComponent.js",
+// 			"node_modules/bad-jsx-in-js-component/index.js",
+// 			"node_modules/bad-jsx-in-js-component/js/BadJSXinJS.js",
+// 			"node_modules/bad-jsx-in-js-component/ts/index.ts",
+// 			"node_modules/bad-jsx-in-js-component/ts/BadTSXinTS.ts",
+// 		],
+// 	},
+// }));
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-	esbuild: {
-		loader: "tsx", // OR "jsx"
-		include: [
-			// Add this for business-as-usual behaviour for .jsx and .tsx files
-			"src/**/*.jsx",
-			"src/**/*.tsx",
-			"node_modules/**/*.jsx",
-			"node_modules/**/*.tsx",
+import react from "@vitejs/plugin-react";
 
-			// Add the specific files you want to allow JSX syntax in
-			"src/LocalJsxInJsComponent.js",
-			"node_modules/bad-jsx-in-js-component/index.js",
-			"node_modules/bad-jsx-in-js-component/js/BadJSXinJS.js",
-			"node_modules/bad-jsx-in-js-component/ts/index.ts",
-			"node_modules/bad-jsx-in-js-component/ts/BadTSXinTS.ts",
-		],
-	},
-}));
+export default defineConfig({
+	plugins: [react()],
+});
